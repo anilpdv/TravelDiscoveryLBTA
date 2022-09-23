4811f9f2d298cd844d05ad81426435fe331f923c
anilpdv <pdvanil007@gmail.com>
2022-09-24 02:14:12 +0530

    Initial Commit

---
+++ TravelDiscoveryLBTA/TravelDiscoveryLBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryLBTAApp.swift
+//  TravelDiscoveryLBTA
+//
+//  Created by anilpdv on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryLBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
